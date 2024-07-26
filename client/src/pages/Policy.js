da76abb0160de37368c686f41b072e979d7ae38c
Rashmisingh <rashmisingh110303@gmail.com>
2024-07-26 20:53:50 +0530

    project2

---
+++ client/src/pages/Policy.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Layout from '../components/Layout/layout';
+
+const Policy = () => {
+  return (
+    < Layout title={'Privacy Policy '}>
+    <div className='row privacypolicy'>
+      <div className='col-md-6'>
+        <img src='/images/privacypolicy.jpg' alt='privacypolicy' style={{width:'100%'}}></img>
+      </div>
+      <div className='col-md-4'>
+        <p className='mt-3'>Add Privacy Policy</p>
+        <p className='mt-3'>Add Privacy Policy</p>
+        <p className='mt-3'>Add Privacy Policy</p>
+        <p className='mt-3'>Add Privacy Policy</p>
+        <p className='mt-3'>Add Privacy Policy</p>
+        <p className='mt-3'>Add Privacy Policy</p>
+      </div>
+    </div>
+    </Layout>
+  )
+}
+
+export default Policy
